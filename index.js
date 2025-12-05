5d87e0a5a24cb628b9eb776123ad8019b65bc4de
devimatsa07 <matsadevi07@gmail.com>
2025-12-05 10:59:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import http from 'http';
+const server=http.createServer((req,res)=>{
+    console.log(req.method);
+    if(req.url==="/users"){
+        res.writeHead(200,{"Content-Type":"application/json"});
+        res.end(JSON.stringify({name:"aditya"}))
+    }
+    else{
+        res.writeHead(200,{'Content-Type':'text/plain'});
+        res.end("hello server port 3007");
+    }
+});
+server.listen(3007,()=>{
+    console.log(`server running on port ${3007}`);
+});
