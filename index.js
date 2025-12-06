6778854602d9a64d422c05fd2ca30db2003206ec
devimatsa07 <matsadevi07@gmail.com>
2025-12-06 10:32:55 +0530

    comiit

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-import http from 'http';
-const server=http.createServer((req,res)=>{
-    console.log(req.method);
-    if(req.url==="/users"){
-        res.writeHead(200,{"Content-Type":"application/json"});
-        res.end(JSON.stringify({name:"aditya"}))
-    }
-    else{
-        res.writeHead(200,{'Content-Type':'text/plain'});
-        res.end("hello server port 3007");
-    }
-});
-server.listen(3007,()=>{
-    console.log(`server running on port ${3007}`);
+// import http from 'http';
+// const server=http.createServer((req,res)=>{
+//     console.log(req.method);
+//     if(req.url==="/users"){
+//         res.writeHead(200,{"Content-Type":"application/json"});
+//         res.end(JSON.stringify({name:"aditya"}))
+//     }
+//     else{
+//         res.writeHead(200,{'Content-Type':'text/plain'});
+//         res.end("hello server port 3007");
+//     }
+// });
+// server.listen(3007,()=>{
+//     console.log(`server running on port ${3007}`);
+// });
+
+
+import express from 'express';
+const app=express();
+app.get('/users',(req,res)=>{
+    res.send("hello this is from backend");
+})
+app.get('/getusers',(req,res)=>{
+    res.send("hello this is from backend");
+})
+app.listen(3007,()=>{
+    console.log("server running on port {3008}");
 });
